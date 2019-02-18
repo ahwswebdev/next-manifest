168b07d33df820298a17689a2dbb9fb98bc74a67
ahwswebdev <ahwswebdev@users.noreply.github.com>
2019-02-18 14:57:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,53 +3,54 @@ const pwaManifest = require('@pwa/manifest');
 const pwaManifestIcons = require('@pwa/manifest-icons');
 
 module.exports = (nextConfig = {}) => {
-  return Object.assign({}, nextConfig, {
-    async webpack(config, options) {
-      const {
-        isServer,
-        dev,
-        buildId,
-        defaultLoaders,
-        config: {
-          distDir
+    return Object.assign({}, nextConfig, {
+        async webpack(config, options) {
+        const {
+            isServer,
+            dev,
+            buildId,
+            defaultLoaders,
+            config: {
+                distDir,
+                publicPath
+            }
+        } = options
+
+        if (!defaultLoaders) {
+            throw new Error(
+                'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
+            )
         }
-      } = options
 
-      if (!defaultLoaders) {
-        throw new Error(
-          'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
-        )
-      }
+        const {webpack, manifest} = nextConfig;
 
-      const {webpack, manifest} = nextConfig;
-
-      if (!isServer && !dev) {
-        const m = await pwaManifest({
-          "background_color": "#FFFFFF",
-          "theme_color": "#FFFFFF",
-          "start_url": "/?utm_source=web_app_manifest",
-          ...manifest
+        if (!isServer && !dev) {
+            const m = await pwaManifest({
+                "background_color": "#FFFFFF",
+                "theme_color": "#FFFFFF",
+                "start_url": "/?utm_source=web_app_manifest",
+                ...manifest
         });
 
-        if (manifest.icons && manifest.icons.src) {
-          m.icons = await pwaManifestIcons({
-            src: manifest.icons.src,
-            cache: manifest.icons.cache || false,
-            output: resolve(process.cwd(), `./static/manifest/icons`),
-            publicPath: '/static/manifest/icons/',
-            sizes: manifest.icons.sizes || [192, 512]
+            if (manifest.icons && manifest.icons.src) {
+                m.icons = await pwaManifestIcons({
+                    src: manifest.icons.src,
+                    cache: manifest.icons.cache || false,
+                    output: resolve(process.cwd(), `./static/manifest/icons`),
+                    publicPath: publicPath ? publicPath :'/static/manifest/icons/',
+                    sizes: manifest.icons.sizes || [192, 512]
 
-          });
+                });
+            }
+
+            await pwaManifest.write('./static/manifest/', m);
         }
 
-        await pwaManifest.write('./static/manifest/', m);
-      }
+        if (typeof webpack === 'function') {
+            return webpack(config, options);
+        }
 
-      if (typeof webpack === 'function') {
-        return webpack(config, options);
-      }
-
-      return config;
+        return config;
     }
-  })
+})
 }
